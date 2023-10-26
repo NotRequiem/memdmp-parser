13880535b389d551420046019e96a454e111240f
Requiem <114197630+NotRequiem@users.noreply.github.com>
2023-10-26 22:50:37 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-#include <regex>
+#include <algorithm>
 
 #define CHUNK_SIZE 4096
 
+bool isASCII(char c) {
+    return (c >= 32 && c <= 126);
+}
+
 int main() {
     std::string filename;
-    char buffer[CHUNK_SIZE];
+    char* buffer = new char[CHUNK_SIZE];
 
     std::cout << "Enter the file path (e.g., D:\\Downloads\\memdump.mem): ";
     std::getline(std::cin, filename);
@@ -15,26 +19,50 @@ int main() {
     std::ifstream file(filename, std::ios::binary);
     if (!file.is_open()) {
         std::cerr << "File open failed" << std::endl;
+        delete[] buffer;
         return 1;
     }
-
-    std::regex pattern("ms-shellactivity:.+\\.exe");
 
     while (!file.eof()) {
         file.read(buffer, CHUNK_SIZE);
         std::streamsize bytesRead = file.gcount();
         if (bytesRead > 0) {
             std::string data(buffer, static_cast<size_t>(bytesRead));
-            std::sregex_iterator it(data.begin(), data.end(), pattern);
-            std::sregex_iterator end;
 
-            while (it != end) {
-                std::smatch match = *it;
-                std::cout << "File execution detected: " << match.str() << std::endl;
-                ++it;
+            size_t pos1, pos2;
+
+            pos1 = data.find("ms-shellactivity");
+            if (pos1 != std::string::npos) {
+                pos2 = data.find(".exe", pos1);
+                if (pos2 != std::string::npos) {
+                    std::string match = data.substr(pos1, pos2 - pos1 + 4);
+                    match.erase(std::remove_if(match.begin(), match.end(), [](char c) { return !isASCII(c) || std::isspace(c); }), match.end());
+                    std::cout << "File execution detected: " << match << std::endl;
+                }
+            }
+
+            pos1 = data.find("IsInVirtualizationContainer");
+            if (pos1 != std::string::npos) {
+                pos2 = data.find(".exe", pos1);
+                if (pos2 != std::string::npos) {
+                    std::string match = data.substr(pos1, pos2 - pos1 + 4);
+                    match.erase(std::remove_if(match.begin(), match.end(), [](char c) { return !isASCII(c) || std::isspace(c); }), match.end());
+                    std::cout << "File execution detected: " << match << std::endl;
+                }
+            }
+
+            pos1 = data.find("[{\"application\"");
+            if (pos1 != std::string::npos) {
+                pos2 = data.find(".exe", pos1);
+                if (pos2 != std::string::npos) {
+                    std::string match = data.substr(pos1, pos2 - pos1 + 4);
+                    match.erase(std::remove_if(match.begin(), match.end(), [](char c) { return !isASCII(c) || std::isspace(c); }), match.end());
+                    std::cout << "File execution detected: " << match << std::endl;
+                }
             }
         }
     }
 
+    delete[] buffer;
     return 0;
 }
