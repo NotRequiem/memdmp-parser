c8d6da36d88b9bb437138f5f3691b9e189e00a2d
Requiem <114197630+NotRequiem@users.noreply.github.com>
2023-10-31 22:52:05 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+#include <windows.h>
 #include <iostream>
 #include <fstream>
 #include <string>
@@ -8,34 +9,49 @@
 #include <algorithm>
 #include <limits>
 
-// Define a constant CHUNK_SIZE for reading data in chunks.
+// Define a constant CHUNK_SIZE for reading data in chunks (resulting in a faster data processing).
 constexpr size_t MIN_CHUNK_SIZE = 330;
 
 // An array to store lowercase conversions of characters.
-std::array<char, 256> toLowerTable;
+std::array<char, 256> lowercaseConversionTable;
 
-// Function to initialize the toLowerTable array.
-void InitializeToLowerTable() {
+/** Platform-independent text color macros
+ * This code defines platform-independent macros to set the text color based on the platform you are running the program on. 
+ * It uses Windows Console API on Windows and ANSI escape codes on non-Windows platforms.
+ */
+ 
+#ifdef _WIN32
+#define SET_TEXT_COLOR_RED() SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 12)
+#define SET_TEXT_COLOR_BLUE() SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9)
+#define RESET_TEXT_COLOR() SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7)
+#else
+#define SET_TEXT_COLOR_RED() std::cout << "\033[31m"
+#define SET_TEXT_COLOR_BLUE() std::cout << "\033[34m"
+#define RESET_TEXT_COLOR() std::cout << "\033[0m"
+#endif
+
+// Function to initialize the lowercaseConversionTable array.
+void InitializeLowercaseConversionTable() {
     for (int i = 0; i < 256; ++i) {
-        toLowerTable[i] = static_cast<char>(std::tolower(static_cast<unsigned char>(i)));
+        lowercaseConversionTable[i] = static_cast<char>(std::tolower(static_cast<unsigned char>(i)));
     }
 }
 
-// Function to convert a character to lowercase using the toLowerTable.
-char ToLower(char c) {
-    return toLowerTable[static_cast<unsigned char>(c)];
+// Function to convert a character to lowercase using the lowercaseConversionTable.
+char ConvertToLowercase(char character) {
+    return lowercaseConversionTable[static_cast<unsigned char>(character)];
 }
 
 // Function to process and clean up strings to be printed.
-void ProcessResults(std::string& str) {
-    size_t length = str.size();
+void CleanStringForPrinting(std::string& inputString) {
+    size_t length = inputString.size();
     size_t writeIndex = 0;
     for (size_t readIndex = 0; readIndex < length; ++readIndex) {
-        char currentChar = str[readIndex];
+        char currentChar = inputString[readIndex];
         switch (currentChar) {
             case '%':
                 if (readIndex + 2 < length) {
-                    switch (str[readIndex + 2]) {
+                    switch (inputString[readIndex + 2]) {
                         case '0':
                             currentChar = ' ';
                             readIndex += 2;
@@ -56,15 +72,15 @@ void ProcessResults(std::string& str) {
                 break;
         }
         if ((currentChar >= 32 && currentChar <= 126) && !std::isspace(currentChar)) {
-            str[writeIndex] = currentChar;
+            inputString[writeIndex] = currentChar;
             ++writeIndex;
         }
     }
-    str.resize(writeIndex);
+    inputString.resize(writeIndex);
 }
 
 // Function to process and clean up a matching string before printing it.
-void ProcessMatch(std::string& match, std::unordered_set<std::string>& printedMatches, char outputChoice, std::unique_ptr<std::ostream>& output) {
+void ProcessMatchingString(std::string& match, std::unordered_set<std::string>& printedMatches, char outputChoice, std::unique_ptr<std::ostream>& output) {
     // Check if the match contains "HarddiskVolume" and replace it with the drive letter.
     if (match.find("HarddiskVolume") != std::string::npos) {
         size_t volumePos = match.find("\\\\Device");
@@ -83,7 +99,7 @@ void ProcessMatch(std::string& match, std::unordered_set<std::string>& printedMa
     }
 
     // Clean up the string further.
-    ProcessResults(match);
+    CleanStringForPrinting(match);
 
     // Check if the match contains "ProgramFiles" and replace it with a more human-readable format.
     if (match.find("ProgramFiles(x86)") != std::string::npos) {
@@ -94,19 +110,29 @@ void ProcessMatch(std::string& match, std::unordered_set<std::string>& printedMa
         match.replace(pos, 12, "Program Files");
     }
 
-    // Check the length of the match and print it if it's short and not previously printed.
-    if (match.length() <= 110 && printedMatches.find(match) == printedMatches.end()) {
+    // Convert the match to lowercase for case-insensitive comparison.
+    std::string lowercaseMatch = match;
+    std::transform(lowercaseMatch.begin(), lowercaseMatch.end(), lowercaseMatch.begin(), ::tolower);
+
+    // Check if the lowercase match has not been previously printed and meets the length condition.
+    if (printedMatches.find(lowercaseMatch) == printedMatches.end() && match.length() <= 110) {
+        // Insert the lowercase match into the set to keep track of it.
+        printedMatches.insert(lowercaseMatch);
+
+        // Print the original match in the desired output.
         if (outputChoice == 'C' || outputChoice == 'c') {
-            std::cout << "Executed file: " << match << std::endl;
+            SET_TEXT_COLOR_BLUE(); // Set text color to blue
+            std::cout << "Executed file: ";
+            RESET_TEXT_COLOR(); // Reset text color
+            std::cout << match << std::endl;
         } else if (outputChoice == 'F' || outputChoice == 'f') {
             (*output) << "Executed file: " << match << std::endl;
         }
-        printedMatches.insert(match);
     }
 }
 
 int main(int argc, char* argv[]) {
-    InitializeToLowerTable();
+    InitializeLowercaseConversionTable();
 
     std::string filename;
     std::vector<char> buffer;
@@ -181,47 +207,64 @@ int main(int argc, char* argv[]) {
     bool done = false;
 
     // Process the memory image in chunks.
-
     while (!done) {
         file.read(buffer.data(), chunkSize);
         std::streamsize bytesRead = file.gcount();
         if (bytesRead > 0) {
             std::string data(buffer.data(), static_cast<size_t>(bytesRead));
+            // Append any remaining overlapData from the previous chunk to the current data.
             data = overlapData + data;
+            // Clear the overlapData variable as it has been processed.
             overlapData.clear();
 
             size_t pos1, pos2;
 
-            /** Search for specific substrings in the data, process and print them.
-            These searches are related to file access or execution evidence.
-            Also, they handle different formats of the same information.
+            /** 
+                Search for specific substrings in the data, process and print them.
+                These searches are related to file access or execution evidence.
+                Also, they handle different formats of the same information.
+            */ 
+
+            /** 
+             * "file:///" -> Explorer string (evidence of file accessed/opened)
+             * "ImageName" -> SgrmBroker string
+             * "AppPath" -> CDPUserSvc and TextInputHost strings
+             * "!!" -> DPS string (indicating compilation time of an executable)
+             * ".exe" and ". e x e" -> ending of any substring (with or without spaces)
             */
 
-            /**
-            "file:///" -> Explorer string (evidence of file accessed/opened)
-            "ImageName" -> SgrmBroker string
-            "AppPath" -> CDPUserSvc and TextInputHost strings
-            "!!" -> DPS string (indicating compilation time of an executable)
-            ".exe" and ". e x e" -> ending of any substring (with or without spaces)
-            */
+            // The matching substrings are passed to ProcessMatchingString for cleaning and printing.
 
-            // The matching substrings are passed to ProcessMatch for cleaning and printing.
-
-            // Handle the "file:///" pattern and variations.
+            // Search for occurrences of "file:///" pattern in the input data.
             pos1 = data.find("file:///");
+
+            // If the pattern is found in the data string.
             if (pos1 != std::string::npos) {
+                // Extract a substring starting from the position of the pattern.
                 auto dataSubstring = data.substr(pos1);
+
+                // Search for the ".exe" pattern within the extracted substring.
                 auto it = std::search(dataSubstring.begin(), dataSubstring.end(), ".exe", ".exe" + 4,
                     [](char a, char b) {
-                        return ToLower(a) == ToLower(b);
+                        return ConvertToLowercase(a) == ConvertToLowercase(b);
                     });
 
+                // If ".exe" is found within the substring.
                 if (it != dataSubstring.end()) {
+                    // Calculate the end position of the matched substring.
                     pos2 = pos1 + static_cast<size_t>(std::distance(dataSubstring.begin(), it));
+
+                    // Extract the matched string, including "file:///" and the ".exe" extension.
                     std::string match = data.substr(pos1 + 8, pos2 - pos1 - 8 + 4);
-                    ProcessMatch(match, printedMatches, outputChoice, output);
+
+                    // Process the matching string using a function named ProcessMatchingString.
+                    ProcessMatchingString(match, printedMatches, outputChoice, output);
                 }
             }
+
+// The same process is repeated for two more patterns: "ImageName" and "AppPath".
+// The code checks for the existence of these patterns in the data string and processes
+// the matching substrings in a similar manner as described above.
 
             // Handle the "ImageName" pattern.
             pos1 = data.find("ImageName");
@@ -229,13 +272,13 @@ int main(int argc, char* argv[]) {
                 auto dataSubstring = data.substr(pos1);
                 auto it = std::search(dataSubstring.begin(), dataSubstring.end(), ".exe", ".exe" + 4,
                     [](char a, char b) {
-                        return ToLower(a) == ToLower(b);
+                        return ConvertToLowercase(a) == ConvertToLowercase(b);
                     });
 
                 if (it != dataSubstring.end()) {
                     pos2 = pos1 + static_cast<size_t>(std::distance(dataSubstring.begin(), it));
                     std::string match = data.substr(pos1 + 12, pos2 - pos1 - 12 + 4);
-                    ProcessMatch(match, printedMatches, outputChoice, output);
+                    ProcessMatchingString(match, printedMatches, outputChoice, output);
                 }
             }
 
@@ -245,13 +288,13 @@ int main(int argc, char* argv[]) {
                 auto dataSubstring = data.substr(pos1);
                 auto it = std::search(dataSubstring.begin(), dataSubstring.end(), ".exe", ".exe" + 4,
                     [](char a, char b) {
-                        return ToLower(a) == ToLower(b);
+                        return ConvertToLowercase(a) == ConvertToLowercase(b);
                     });
 
                 if (it != dataSubstring.end()) {
                     pos2 = pos1 + static_cast<size_t>(std::distance(dataSubstring.begin(), it));
                     std::string match = data.substr(pos1 + 12, pos2 - pos1 - 12 + 4);
-                    ProcessMatch(match, printedMatches, outputChoice, output);
+                    ProcessMatchingString(match, printedMatches, outputChoice, output);
                 }
             }
 
@@ -280,11 +323,12 @@ int main(int argc, char* argv[]) {
 
                     if (endPos - start >= ((pos2 == data.find(searchStringWithSpaces, pos1)) ? 7 : 4)) {
                         std::string match = data.substr(start, endPos - start);
-                        ProcessMatch(match, printedMatches, outputChoice, output);
+                        ProcessMatchingString(match, printedMatches, outputChoice, output);
                     }
                 }
             }
             // Store the last part of data (220 characters) for overlap with the next chunk.
+            // This ensures that any partial information at the end of the chunk is retained for the next iteration.
             overlapData = data.substr(data.size() - 220);
         } else {
             done = true;
@@ -297,7 +341,10 @@ int main(int argc, char* argv[]) {
             printedMatch[1] == ':' && printedMatch[2] == '\\') {
             if (!std::ifstream(printedMatch)) {
                 if (outputChoice == 'C' || outputChoice == 'c') {
-                    std::cout << "Deleted file (file could not be found): " << printedMatch << std::endl;
+                    SET_TEXT_COLOR_RED(); // Set text color to red
+                    std::cout << "Deleted file (file could not be found): ";
+                    RESET_TEXT_COLOR(); // Reset text color
+                    std::cout << printedMatch << std::endl;
                 } else if (outputChoice == 'F' || outputChoice == 'f') {
                     (*output) << "Deleted file (file could not be found): " << printedMatch << std::endl;
                 }
